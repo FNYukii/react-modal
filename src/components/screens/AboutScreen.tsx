3650b2c00bc195c73327073e9a57d4e44c09bf5f
Yu357 <forestgreen14689@gmail.com>
2022-10-30 02:39:32 +0900

    feat: 画像を配置

--- src/components/screens/AboutScreen.tsx
+++ src/components/screens/AboutScreen.tsx
@@ -10,6 +10,7 @@ function AboutScreen() {
             <main className="px-3">
 
                 <h1 className="font-bold text-xl">About</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam, quisquam. Provident cumque iusto voluptatem facilis laboriosam. Quasi, placeat quis? Officiis qui vitae perferendis odio nemo quam temporibus quos voluptatibus sint provident magni numquam non laborum reprehenderit, illo dolorem eius blanditiis et, facere sapiente similique est quasi eveniet accusamus? Necessitatibus, doloribus!</p>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium sint praesentium cumque delectus quidem officia sunt? Sed quibusdam error iste porro voluptatum doloremque ut cum odio consequatur hic distinctio perspiciatis accusantium ipsum sit dolor laboriosam temporibus delectus exercitationem, aspernatur corrupti. Eos ratione, harum modi, reprehenderit tempore adipisci aperiam atque sapiente, facere nesciunt beatae recusandae illum? Quis expedita, deleniti iure cupiditate nesciunt vel dignissimos dolores blanditiis molestiae cumque modi sapiente placeat optio ad non et odio. Cum, illum magni voluptatum officiis reiciendis explicabo, sint totam laboriosam facere quisquam ex animi veniam autem natus sapiente in aperiam! Corporis totam corrupti odio perspiciatis.</p>
             </main>
         </div>
--- src/components/screens/TopScreen.tsx
+++ src/components/screens/TopScreen.tsx
@@ -10,7 +10,14 @@ function TopScreen() {
             <main className="px-3">
 
                 <h1 className="font-bold text-xl">Top</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium sint praesentium cumque delectus quidem officia sunt? Sed quibusdam error iste porro voluptatum doloremque ut cum odio consequatur hic distinctio perspiciatis accusantium ipsum sit dolor laboriosam temporibus delectus exercitationem, aspernatur corrupti. Eos ratione, harum modi, reprehenderit tempore adipisci aperiam atque sapiente, facere nesciunt beatae recusandae illum? Quis expedita, deleniti iure cupiditate nesciunt vel dignissimos dolores blanditiis molestiae cumque modi sapiente placeat optio ad non et odio. Cum, illum magni voluptatum officiis reiciendis explicabo, sint totam laboriosam facere quisquam ex animi veniam autem natus sapiente in aperiam! Corporis totam corrupti odio perspiciatis.</p>
+                <p mt-5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio itaque qui nihil iste esse ipsa impedit quia, molestiae officia recusandae hic inventore unde facilis eaque debitis est. Dolores, aperiam voluptatibus!</p>
+                
+                <div className="mt-5 flex gap-6">
+
+                    <img src="https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Forest" className="aspect-video w-96"/>
+                    <img src="https://images.unsplash.com/photo-1437482078695-73f5ca6c96e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="River" className="aspect-video w-96"/>
+                    <img src="https://images.unsplash.com/photo-1632167213406-9537c9494ed8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" alt="River" className="aspect-video w-96"/>
+                </div>
             </main>
         </div>
     )
