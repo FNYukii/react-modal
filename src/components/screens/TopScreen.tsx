debc2922c76dcd36b88e68b3cc154d7ea00e9b67
Yu357 <forestgreen14689@gmail.com>
2022-10-30 02:48:22 +0900

    feat: 画像にホバー状態を設定

--- src/components/screens/TopScreen.tsx
+++ src/components/screens/TopScreen.tsx
@@ -17,15 +17,15 @@ function TopScreen() {
 
                 <div className="mt-5 flex gap-6">
 
-                    <Link to='/images/forest' state={{ previousPath: location.pathname }}>
+                    <Link to='/images/forest' state={{ previousPath: location.pathname }} className="hover:opacity-90 transition">
                         <img src="https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Forest" className="aspect-video w-96" />
                     </Link>
 
-                    <Link to='/images/river' state={{ previousPath: location.pathname }}>
+                    <Link to='/images/river' state={{ previousPath: location.pathname }} className="hover:opacity-90 transition">
                         <img src="https://images.unsplash.com/photo-1437482078695-73f5ca6c96e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="River" className="aspect-video w-96" />
                     </Link>
 
-                    <Link to='/images/mountains' state={{ previousPath: location.pathname }}>
+                    <Link to='/images/mountains' state={{ previousPath: location.pathname }} className="hover:opacity-90 transition">
                         <img src="https://images.unsplash.com/photo-1632167213406-9537c9494ed8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" alt="Mountains" className="aspect-video w-96" />
                     </Link>
                 </div>
